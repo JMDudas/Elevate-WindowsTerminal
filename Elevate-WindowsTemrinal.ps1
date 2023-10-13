ce5d99decc2d46b0205389c44c2337c384b3c100
JMDudas <jmdudas@protonmail.com>
2023-10-13 05:18:18 -0600

    Removed duplicate start-process line that was causing 2 admin terminals to open. Removed exit line and uncommented Stop-Process to close the non-admin terminal

--- Elevate-WindowsTemrinal.ps1
+++ Elevate-WindowsTemrinal.ps1
@@ -9,13 +9,12 @@ class WindowsTerminal{
 $nonadminTerminal = [WindowsTerminal]::new()
 # Get the current Windows Terminal Process ID - needed to close the non-admin terminal after the Admin terminal is started
 $nonadminTerminal.PID = get-process | Where-Object {$_.Name -eq $nonadminTerminal.ProcessName} | Select-Object -ExpandProperty ID
-# Start a new Windows Terminal with Admin permissions
-start-process -FilePath $nonadminTerminal.CommandName -Verb Runas
 # Get the Process ID of the Admin Windows Terminal
 try {
+    # Start a new Windows Terminal with Admin permissions
     start-process -FilePath $nonadminTerminal.CommandName -Verb Runas
-    exit
-    # Stop-Process -Id $nonadminTerminal.PID -Force 
+    # End the non-admin Windows Terminal
+    Stop-Process -Id $nonadminTerminal.PID -Force 
 }
 catch {
     Write-Host "Failed to open Windows Terminal with Admin Permissions `n $Error"
