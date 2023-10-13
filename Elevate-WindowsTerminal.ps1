39e10d67e93e4e0f446c1ef8e154217ec56fa680
JMDudas <jmdudas@protonmail.com>
2023-10-13 06:18:25 -0600

    Corrected spelling mistake in script name

---
+++ Elevate-WindowsTerminal.ps1
@@ -0,0 +1,21 @@
+# SCRIPT PURPOSE: Open an Admin Windows Terminal from a non-admin Windows Terminal
+class WindowsTerminal{
+    [string]$ProcessName = "WindowsTerminal" # The name of Windows Terminal Process shown in GET-PROCESS results [as of 10/13/23 the process name is WindowsTermainal]
+    [string]$CommandName = "wt" # The command to run that opens/starts a new Windows Terminal [as of 10/13/23 the command is 'wt']
+    [int32]$PID # Process ID show in GET-PROCESS results
+}
+
+# Create a new WindowsTerminal Object
+$nonadminTerminal = [WindowsTerminal]::new()
+# Get the current Windows Terminal Process ID - needed to close the non-admin terminal after the Admin terminal is started
+$nonadminTerminal.PID = get-process | Where-Object {$_.Name -eq $nonadminTerminal.ProcessName} | Select-Object -ExpandProperty ID
+# Get the Process ID of the Admin Windows Terminal
+try {
+    # Start a new Windows Terminal with Admin permissions
+    start-process -FilePath $nonadminTerminal.CommandName -Verb Runas
+    # End the non-admin Windows Terminal
+    Stop-Process -Id $nonadminTerminal.PID -Force 
+}
+catch {
+    Write-Host "Failed to open Windows Terminal with Admin Permissions `n $Error"
+}
